812e319cf27716a3e30e5ae2a75fc4b7b5e2b46d
111360250 <t111360250@ntut.org.tw>
2022-10-22 08:07:47 +0800

    first

---
+++ P13/P13/P13/Main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double Power(double, int);
+void main()
+{
+	int k; double ans;
+	printf("計算3.5的k次方?請輸入k=");
+	scanf("%d", &k);
+	ans = Power(3.5, k);
+	printf("3.5的%d次方=%f\n", k, ans);
+	system("pause");
+}
+double Power(double x, int n)
+{
+	int i; double a=1;
+	for (i = 1; i <= n; i++)
+		a = a * x;
+	return a;
+}
